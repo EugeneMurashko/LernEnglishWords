00814e91b6d2225e0cefd374773efb3657b861d6
EugeneMurahsko <eugen.murashko@gmail.com>
2016-12-25 03:49:54 +0300

    Repository Метод Test с примерами всех манипуляций с данными.

--- LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
+++ LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LernEnglishWords.Models;
+using System.Data.Entity;
 
 namespace LernEnglishWords.Controllers
 {
@@ -25,6 +27,44 @@ namespace LernEnglishWords.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Temp()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        // Метод со всеми видами манипуляций с данными
+        private void Test()
+        {
+            var EnglishWords = Repository.Select<EnglishWord>().ToList();
+
+            EnglishWord EngWord = new EnglishWord { Word = "TEST_WORD" };
+            Repository.Insert<EnglishWord>(EngWord);
+            var EnglishWords2 = Repository.Select<EnglishWord>().ToList();
+
+            var EnglishWordTest = EnglishWords2
+                .Where(c => c.Word.StartsWith("TEST_WORD")).ToList();
+            foreach(EnglishWord b in EnglishWordTest)
+            {
+                b.Word = "DELETE";
+                Repository.Update<EnglishWord>(b);
+            }
+
+            var EnglishWords3 = Repository.Select<EnglishWord>().ToList();
+            var EnglishWordDelete = EnglishWords2
+                .Where(c => c.Word.StartsWith("DELETE")).ToList();
+            foreach (EnglishWord b in EnglishWordDelete)
+            {
+                Repository.Delete<EnglishWord>(b);
+            }
+
+            var EnglishWords4 = Repository.Select<EnglishWord>().ToList();
+            var EnglishWordTest3 = EnglishWords4
+               .Where(c => c.Word.StartsWith("TEST_WORD")).ToList();
+            var EnglishWordDelete3 = EnglishWords4
+                .Where(c => c.Word.StartsWith("DELETE")).ToList();
         }
     }
 }
---
+++ LernEnglishWords/LernEnglishWords/Models/Repository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LernEnglishWords.Models
+{
+    public class Repository
+    {
+        public static IQueryable<TEntity> Select<TEntity>()
+            where TEntity : class
+        {
+            LernEnglishContext context = new LernEnglishContext();
+
+            // Здесь мы можем указывать различные настройки контекста,
+            // например выводить в отладчик сгенерированный SQL-код
+            context.Database.Log =
+                (s => System.Diagnostics.Debug.WriteLine(s));
+
+            // Загрузка данных с помощью универсального метода Set
+            return context.Set<TEntity>();
+        }
+
+        public static void Insert<TEntity>(TEntity entity) where TEntity : class
+        {
+            // Настройки контекста
+            LernEnglishContext context = new LernEnglishContext();
+            context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+            context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public static void Inserts<TEntity>(IEnumerable<TEntity> entities) where TEntity : class
+        {
+            // Настройки контекста
+            LernEnglishContext context = new LernEnglishContext();
+
+            // Отключаем отслеживание и проверку изменений для оптимизации вставки множества полей
+            context.Configuration.AutoDetectChangesEnabled = false;
+            context.Configuration.ValidateOnSaveEnabled = false;
+
+            context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+
+            foreach (TEntity entity in entities)
+                context.Entry(entity).State = EntityState.Added;
+            context.SaveChanges();
+
+            context.Configuration.AutoDetectChangesEnabled = true;
+            context.Configuration.ValidateOnSaveEnabled = true;
+        }
+ 
+        public static void Update<TEntity>(TEntity entity)
+    where TEntity : class
+        {
+            // Настройки контекста
+            LernEnglishContext context = new LernEnglishContext();
+            context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+            context.Entry<TEntity>(entity).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public static void Delete<TEntity>(TEntity entity)
+    where TEntity : class
+        {
+            // Настройки контекста
+            LernEnglishContext context = new LernEnglishContext();
+            context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+            context.Entry<TEntity>(entity).State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+    }
+}
+
+//вариант из учебника. Здесь почемуто соединение передается как параметр
+// хотя в этом есть смысл, если у нашего сайта было бы несколько соенидений с разными бд
+/*public static void Update<TEntity>(TEntity entity, DbContext context)
+where TEntity : class
+{
+    // Настройки контекста
+    context.Database.Log = (s => System.Diagnostics.Debug.WriteLine(s));
+
+    context.Entry<TEntity>(entity).State = EntityState.Modified;
+    context.SaveChanges();
+}*/
+
+// перегруженный вариант Update, по аналогии с остальными
+
+/* обработчик позваляющий отслеживать изменения (удаления и добавления) локальных данных
+public static void LocalLinqQueryies()
+{
+    SampleContext context = new SampleContext();
+
+    context.Customers.Local
+        .CollectionChanged += (sender, args) => // лямбда-выражение. Можно добавить и отдельный метод
+        {
+            if (args.NewItems != null)
+            {
+                foreach (Customer c in args.NewItems)
+                {
+                    Console.WriteLine("Добавлен: " + c.FirstName);
+                }
+            }
+            if (args.OldItems != null)
+            {
+                foreach (Customer c in args.OldItems)
+                {
+                    Console.WriteLine("Удален: " + c.FirstName);
+                }
+            }
+        };
+
+    context.Customers.Load();
+}*/
