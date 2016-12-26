d362f50d98904a215d940c808e732e3ccc17f940
EugeneMurahsko <eugen.murashko@gmail.com>
2016-12-26 20:23:01 +0300

    Удалил метод Test чтобы сделать cherry-pick только с Repository (reverted from commit 53435e2422eaf9e4d2d6bb472dc844882c81d033)

--- LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
+++ LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
@@ -28,6 +28,38 @@ namespace LernEnglishWords.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }  
+        }
+
+        // Метод со всеми видами манипуляций с данными
+        private void Test()
+        {
+            var EnglishWords = Repository.Select<EnglishWord>().ToList();
+
+            EnglishWord EngWord = new EnglishWord { Word = "TEST_WORD" };
+            Repository.Insert<EnglishWord>(EngWord);
+            var EnglishWords2 = Repository.Select<EnglishWord>().ToList();
+
+            var EnglishWordTest = EnglishWords2
+                .Where(c => c.Word.StartsWith("TEST_WORD")).ToList();
+            foreach (EnglishWord b in EnglishWordTest)
+            {
+                b.Word = "DELETE";
+                Repository.Update<EnglishWord>(b);
+            }
+
+            var EnglishWords3 = Repository.Select<EnglishWord>().ToList();
+            var EnglishWordDelete = EnglishWords2
+                .Where(c => c.Word.StartsWith("DELETE")).ToList();
+            foreach (EnglishWord b in EnglishWordDelete)
+            {
+                Repository.Delete<EnglishWord>(b);
+            }
+
+            var EnglishWords4 = Repository.Select<EnglishWord>().ToList();
+            var EnglishWordTest3 = EnglishWords4
+               .Where(c => c.Word.StartsWith("TEST_WORD")).ToList();
+            var EnglishWordDelete3 = EnglishWords4
+                .Where(c => c.Word.StartsWith("DELETE")).ToList();
+        }    
     }
 }
