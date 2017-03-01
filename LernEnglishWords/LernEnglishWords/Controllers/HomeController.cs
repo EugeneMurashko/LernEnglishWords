cbee343c9a520aef1a6181e6f44239c1e9ec42e0
EugeneMurahsko <eugen.murashko@gmail.com>
2017-03-01 22:16:24 +0300

    В хомконтролер добавлен закомнченый код, который хирургическим путем отрезвляет миграции

--- LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
+++ LernEnglishWords/LernEnglishWords/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ using System.Web.Mvc;
 using LernEnglishWords.Models;
 using System.Data.Entity;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.IO;
+using System.Data.Entity.Infrastructure;
+using System.IO.Compression;
+using System.Data.SqlClient;
+
 
 namespace LernEnglishWords.Controllers
 {
@@ -30,4 +36,70 @@ namespace LernEnglishWords.Controllers
             return View();
         }
     }
+    // когда бд не хочет мигрировать
+    // использовать осторожно!!!
+   /* public static class HelpClass
+    {
+        public static void Temp()
+        {
+            Testing context = new Testing();
+            MemoryStream stream = new MemoryStream();
+            using (var xmlWriter = XmlWriter.Create(stream, new XmlWriterSettings { Indent = true }))
+            {
+                EdmxWriter.WriteEdmx(context, xmlWriter);
+            }
+
+            using (var gzip = new GZipStream(stream, CompressionMode.Compress))
+            using (var xmlWriter = XmlWriter.Create(gzip, new XmlWriterSettings { Indent = true }))
+            {
+                EdmxWriter.WriteEdmx(context, xmlWriter);
+            }
+        }
+
+        public static void ForceUpdate(this Testing context)
+        {
+            using (var connection = ((SqlConnection)context.Database.Connection))
+            {
+                var migrationId = string.Empty;
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = @"select top 1 MigrationId
+                                      from __MigrationHistory
+                                     order by MigrationId desc";
+                    connection.Open();
+                    migrationId = command.ExecuteScalar().ToString();
+                    connection.Close();
+                }
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText =
+                        @"
+                 update __MigrationHistory
+                    set Model = @Model
+                  where MigrationId = @MigrationId;
+                ";
+                    command.Parameters.AddWithValue("@Model", GetModel(context));
+                    command.Parameters.AddWithValue("@MigrationId", migrationId);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static byte[] GetModel(Testing context)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var gzip = new GZipStream(stream, CompressionMode.Compress))
+                using (var xml = XmlWriter.Create(gzip, new XmlWriterSettings { Indent = true }))
+                {
+                    EdmxWriter.WriteEdmx(context, xml);
+                }
+
+                return stream.ToArray();
+            }
+        }
+    }*/
 }
