7e1be3c3994ddbd0af70060bf7e87490cf425f7f
EugeneMurahsko <eugen.murashko@gmail.com>
2017-03-01 22:11:25 +0300

    Миграции. Тут все автоматизировано.

---
+++ LernEnglishWords/LernEnglishWords/Migrations/201702191332023_TestingMigrations.cs
@@ -0,0 +1,18 @@
+namespace LernEnglishWords.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestingMigrations : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+         
+        }
+    }
+}
---
+++ LernEnglishWords/LernEnglishWords/Migrations/201702191721289_TestingMigrations1.cs
@@ -0,0 +1,16 @@
+namespace LernEnglishWords.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestingMigrations1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
